fdc9ce671642fd98a460fecfd2a29e9706e5e764
Connor T Werdel <75349974+c-werdel@users.noreply.github.com>
2023-01-25 14:54:34 -0700

    Update lambda_greeting.py
    new code change test

--- lambda_greeting.py
+++ lambda_greeting.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
 			"type": 'section',
 			"text": {
 				"type": "mrkdwn",
-				"text": "Good Morning: Cascade Team"
+				"text": "Good Afternoon: Cascade Team"
 			}
 		}
 	]
