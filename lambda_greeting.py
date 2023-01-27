a72c145a0b4e29f95c988e0764e2f1bcedc9c27c
Connor T Werdel <75349974+c-werdel@users.noreply.github.com>
2023-01-26 17:00:51 -0700

    Update lambda_greeting.py
    testing out an image attachment to slackbot

--- lambda_greeting.py
+++ lambda_greeting.py
@@ -8,12 +8,22 @@ def lambda_handler(event, context):
     hook_url = os.environ["webhook"]
 
     payload = {
-	"blocks": [
+# 	"blocks": [
+# 		{
+# 			"type": 'section',
+# 			"text": {
+# 				"type": "mrkdwn",
+# 				"text": "Good Morning: Cascade Team"
+# 				{
+	 "attachments": [
 		{
-			"type": 'section',
-			"text": {
-				"type": "mrkdwn",
-				"text": "Good Morning: Cascade Team"
+		    "fallback": "Error could not show image.",
+		    "text": "Good Morning: Cascade Team"
+		    "image_url": "https://dribbble.com/shots/3502310-Toolio/attachments/9861572?mode=media",
+		    "thumb_url": "http://example.com/path/to/thumb.png"
+		}
+	    ]
+	}
 			}
 		}
 	]
